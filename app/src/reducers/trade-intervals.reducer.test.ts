e95accbd26c2f7110bcd2a0a7c2bb95cc4d25318
Sergo <rogaldh@radsh.red>
2022-10-13 02:58:43 +0300

    Fix tests

--- app/src/reducers/trade-intervals.reducer.test.ts
+++ app/src/reducers/trade-intervals.reducer.test.ts
@@ -13,6 +13,7 @@ describe("trade-intervals", () => {
             left: tif,
             index,
           })),
+          selectedTif: [undefined, -1],
         })
       )
     ).toEqual({
@@ -21,6 +22,7 @@ describe("trade-intervals", () => {
         { tif: 2, left: 2, index: 1 },
         { tif: 3, left: 3, index: 2 },
       ],
+      pairSelected: [undefined, -1],
       periodTifs: [1, 2, 3],
       scheduleTifs: [-1, 1, 2, 3],
       tifsLeft: [1, 2, 3],
@@ -29,6 +31,7 @@ describe("trade-intervals", () => {
 
   it("should change intervals", () => {
     const state = {
+      pairSelected: [2, undefined],
       periodTifs: [1, 2, 3],
       scheduleTifs: [-1, 2, 3, 4],
       tifsLeft: [1, 2, 3],
