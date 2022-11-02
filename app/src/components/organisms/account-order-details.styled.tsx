429ebbefcb768229f928fd5a2682e35917e08bf7
Sergo <rogaldh@radsh.red>
2022-11-02 23:47:42 +0400

    Fix errors

--- app/src/components/organisms/account-order-details.styled.tsx
+++ app/src/components/organisms/account-order-details.styled.tsx
@@ -1,6 +1,4 @@
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 
 export const Container = styled(Box)`
