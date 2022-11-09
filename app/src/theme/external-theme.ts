ab4c4270762a90eedbf38ebd37eca443e87c212c
Sergo <rogaldh@radsh.red>
2022-11-09 12:54:58 +0300

    Fix theme types

--- app/src/theme/external-theme.ts
+++ app/src/theme/external-theme.ts
@@ -17,6 +17,19 @@ declare module "@mui/material/styles" {
       600: true;
     };
   }
+  interface PaletteOptions {
+    neutral: {
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+    };
+  }
 }
 
 export const theme = createTheme({
