3873aacae0b8b87c9532970101622f6f79d9ab4a
Sergo <rogaldh@radsh.red>
2022-11-17 19:36:03 +0300

    Adjust stack position according the viewport

--- app/src/contexts/notification-context.tsx
+++ app/src/contexts/notification-context.tsx
@@ -1,6 +1,8 @@
 import type { ReactElement } from "react";
 import Fade from "@mui/material/Fade";
+import { useMemo } from "react";
 import { SnackbarProvider, useSnackbar } from "notistack";
+import useBreakpoints from "../hooks/use-breakpoints";
 
 export interface Props {
   maxSnack?: number;
@@ -9,16 +11,26 @@ export interface Props {
 
 export { useSnackbar };
 
-export const NotificationProvider = ({ maxSnack = 1, children }: Props) => (
-  <SnackbarProvider
-    anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "right",
-    }}
-    autoHideDuration={3000}
-    maxSnack={maxSnack}
-    TransitionComponent={Fade}
-  >
-    {children}
-  </SnackbarProvider>
-);
+export const NotificationProvider = ({ maxSnack = 1, children }: Props) => {
+  const { isMobile } = useBreakpoints();
+
+  const anchorOrigin: { vertical: "top" | "bottom"; horizontal: "right" } =
+    useMemo(
+      () =>
+        isMobile
+          ? { vertical: "top", horizontal: "right" }
+          : { vertical: "bottom", horizontal: "right" },
+      [isMobile]
+    );
+
+  return (
+    <SnackbarProvider
+      anchorOrigin={anchorOrigin}
+      autoHideDuration={3000}
+      maxSnack={maxSnack}
+      TransitionComponent={Fade}
+    >
+      {children}
+    </SnackbarProvider>
+  );
+};
