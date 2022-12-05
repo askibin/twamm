3c30f52dd57ae1b90391699662c92f872c40b2f3
Sergo <rogaldh@radsh.red>
2022-12-06 00:09:17 +0300

    Improve script

--- app/scripts/fetch_idl.bash
+++ app/scripts/fetch_idl.bash
@@ -13,10 +13,9 @@ if [[ "$(uname -s)" == "1Linux" ]]; then
 apt-get update && apt-get upgrade && apt-get install -y pkg-config build-essential libudev-dev libssl-dev
 fi
 
-if [[ -x "which -s anchor" ]]; then
+if [[ -x "$(which -s anchor)" ]]; then
   echo "Skip installation. Anchor was already installed"
 else
-  curl https://sh.rustup.rs -sSf | sh
   cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
 
   avm install latest
