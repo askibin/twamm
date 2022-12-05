ff66a7da014bcd5fa9a5017b046e2339625e5638
Sergo <rogaldh@radsh.red>
2022-12-05 23:55:43 +0300

    Improve script

--- app/scripts/fetch_idl.bash
+++ app/scripts/fetch_idl.bash
@@ -9,11 +9,12 @@ fi
 #npx @project-serum/anchor-cli --provider.cluster https://api.mainnet-beta.solana.com idl fetch ${NEXT_PUBLIC_PROGRAM_ADDRESS} > ./idl.json
 #
 
-if [[ "$(uname -s)" == "Linux" ]]; then
+if [[ "$(uname -s)" == "1Linux" ]]; then
 apt-get update && apt-get upgrade && apt-get install -y pkg-config build-essential libudev-dev libssl-dev
 fi
 
 if [[ -x "which -s anchor" ]]; then
+  echo "Skip installation. Anchor was already installed"
 else
   cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
 
