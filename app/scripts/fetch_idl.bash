173ae64cc199423d27fbf930112de7afb5165b0a
Sergo <rogaldh@radsh.red>
2022-12-05 23:40:41 +0300

    Improve script

--- app/scripts/fetch_idl.bash
+++ app/scripts/fetch_idl.bash
@@ -2,6 +2,21 @@
 ENVFILE=.env.local
 if [[ -f "$ENVFILE" ]]; then
   source "$ENVFILE"
+  cat .env.local
 fi
 
-npx @project-serum/anchor-cli --provider.cluster https://api.mainnet-beta.solana.com idl fetch ${NEXT_PUBLIC_PROGRAM_ADDRESS} > ./idl.json
+which cargo
+
+#npx @project-serum/anchor-cli --provider.cluster https://api.mainnet-beta.solana.com idl fetch ${NEXT_PUBLIC_PROGRAM_ADDRESS} > ./idl.json
+#
+
+if [[ "$(uname -a)" == "Linux" ]]; then
+apt-get update && apt-get upgrade && apt-get install -y pkg-config build-essential libudev-dev libssl-dev
+fi
+
+cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
+
+avm install latest
+avm use latest
+
+anchor --version
