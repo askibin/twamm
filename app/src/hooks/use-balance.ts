9ee50ed82520a8b281a1645dc71ea41d8614abea
Sergo <rogaldh@radsh.red>
2022-12-06 21:53:49 +0300

    Improve balance fetching

--- app/src/hooks/use-balance.ts
+++ app/src/hooks/use-balance.ts
@@ -1,42 +1,15 @@
 import type { Provider } from "@project-serum/anchor";
 import type { PublicKey } from "@solana/web3.js";
+import Maybe, { Extra } from "easy-maybe/lib";
 import useSWR from "swr";
 import { SplToken } from "@twamm/client.js/lib/spl-token";
 import { useWallet } from "@solana/wallet-adapter-react";
+import useAccountTokens, { AccountBalance } from "./use-account-tokens";
 import useProgram from "./use-program";
 
-type Params = { address: PublicKey; mint: string };
+const { andMap, withDefault, of } = Maybe;
 
-type BalanceData = {
-  account: {
-    executable: boolean;
-    lamports: number;
-    rentEpoch?: number;
-    owner: PublicKey;
-    data: {
-      program: "spl-token";
-      space: number;
-      parsed: {
-        type: "account";
-        info: {
-          isNative: boolean;
-          mint: string;
-          owner: string;
-          state: string;
-          tokenAmount: {
-            amount: string;
-            decimals: number;
-            uiAmount: number;
-            uiAmountString: string;
-          };
-        };
-      };
-    };
-  };
-  pubkey: PublicKey;
-};
-
-const isNative = (a: string) => SplToken.isNativeAddress(a);
+type Params = { address: PublicKey; balances: AccountBalance[]; mint: string };
 
 const swrKey = (params: Params) => ({
   key: "balance",
@@ -45,29 +18,17 @@ const swrKey = (params: Params) => ({
 
 const fetcher =
   ({ provider }: { provider: Provider }) =>
-  async ({ params }: { params: Params }) => {
-    if (isNative(params.mint)) {
-      const data: number = await provider.connection.getBalance(params.address);
-      return data * 1e-9;
+  async (swr: FetcherArgs<Params>) => {
+    if (SplToken.isNativeAddress(swr.params.mint)) {
+      const data: number = await provider.connection.getBalance(
+        swr.params.address
+      );
+
+      return Number((data * 1e-9).toFixed(String(data).length));
     }
 
-    const data = (await provider.connection.getParsedProgramAccounts(
-      SplToken.getProgramId(),
-      {
-        filters: [
-          { dataSize: 165 },
-          {
-            memcmp: {
-              offset: 32,
-              bytes: params.address.toBase58(),
-            },
-          },
-        ],
-      }
-    )) as BalanceData[];
-
-    const accounts = data.map((d) => d.account.data.parsed.info);
-    const targetInfo = accounts.find((a) => a.mint === params.mint);
+    const accounts = swr.params.balances.map((d) => d.account.data.parsed.info);
+    const targetInfo = accounts.find((a) => a.mint === swr.params.mint);
 
     if (!targetInfo) return 0;
 
@@ -78,8 +39,16 @@ export default (mint: Voidable<string>, options = {}) => {
   const { publicKey: address } = useWallet();
   const { provider } = useProgram();
 
+  const accountTokens = useAccountTokens();
+
   return useSWR(
-    address && mint && swrKey({ address, mint }),
+    withDefault(
+      undefined,
+      andMap(
+        (a) => swrKey({ address: a[0], balances: a[1], mint: a[2] }),
+        Extra.combine3([of(address), of(accountTokens.data), of(mint)])
+      )
+    ),
     fetcher({ provider }),
     options
   );
