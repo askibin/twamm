9871235c99a02b27785ee20cccc14063583dda3e
Sergo <rogaldh@radsh.red>
2022-12-27 19:55:48 +0300

    Improve LogViewer styles

--- app/src/organisms/log-viewer.styled.tsx
+++ app/src/organisms/log-viewer.styled.tsx
@@ -6,6 +6,20 @@ export const Logs = styled(List)`
   border: 1px solid ${(p) => p.theme.palette.text.secondary};
   border-radius: ${(p) => p.theme.shape.borderRadius}px;
   overflow-x: scroll;
+  width: ${(p) => p.theme.breakpoints.values.tablet - 90}px;
+  /* adjust width to smooth the layout change */
+  height: 40vh;
+`;
+
+export const MobileLogs = styled(List)`
+  border: 1px solid ${(p) => p.theme.palette.text.secondary};
+  border-radius: ${(p) => p.theme.shape.borderRadius}px;
+  overflow-x: scroll;
+  max-height: 40vh;
+
+  & .MuiTypography-root {
+    font-family: monospace, sans-serif;
+  }
 `;
 
 export const LogRecord = styled(ListItemText)`
