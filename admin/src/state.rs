539a0857792a07aab2fdb221b5b20620a97d5059
Sergo <rogaldh@radsh.red>
2023-05-03 10:52:04 +0300

    Add the admin client

---
+++ admin/src/state.rs
@@ -0,0 +1,11 @@
+// by token_pair
+// by wallet
+// by both
+pub fn list_orders() {}
+
+pub fn list_token_pairs() {}
+
+pub fn list_pools() {}
+
+pub fn list_multisig() {}
+
---
+++ admin/src/test.rs
@@ -0,0 +1,5 @@
+#[cfg(test)]
+mod instructions {
+    #[test]
+    fn should_init() {}
+}
